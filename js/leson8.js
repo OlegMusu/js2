bedcfda91670665422aac0ea147c23ac0d325fe3
OlegMusu <olegmusulevskyi@gmail.com>
2025-04-11 21:05:17 +0300

    js

---
+++ js/leson8.js
@@ -0,0 +1,3 @@
+let number = prompt("Введи число");
+let sqrtNumber = Math.sqrt(number);
+console.log(sqrtNumber)
---
+++ js/leson9.js
@@ -0,0 +1,6 @@
+const integer = 42;
+const stringNumber = "256";
+let convertedInt = parseInt(integer);
+console.log(convertedInt)
+let convertedString = toString(stringNumber);
+console.log(convertedString)
